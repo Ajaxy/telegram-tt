6d3adb417ac1dd1b4c9d574a8983c968250d6c9f
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-04-18 22:41:18 +0300

    Localization: Fix translations are not applied sometimes

--- src/hooks/useLang.ts
+++ src/hooks/useLang.ts
@@ -1,16 +1,15 @@
-import { useEffect } from '../lib/teact/teact';
-
 import { ApiMediaFormat } from '../api/types';
 
 import * as langProvider from '../util/langProvider';
 import useForceUpdate from './useForceUpdate';
+import useOnChange from './useOnChange';
 
 export type LangFn = typeof langProvider.getTranslation;
 
 export default <T extends ApiMediaFormat = ApiMediaFormat.BlobUrl>(): LangFn => {
   const forceUpdate = useForceUpdate();
 
-  useEffect(() => {
+  useOnChange(() => {
     return langProvider.addCallback(forceUpdate);
   }, [forceUpdate]);
 
