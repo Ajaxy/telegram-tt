6c30c2d665a9f6376e02ddef40c1b0aca4d88b04
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-04-19 18:37:06 +0300

    Middle Footer: Various issues (#1031)

--- src/components/middle/helpers/calculateMiddleFooterTransforms.ts
+++ src/components/middle/helpers/calculateMiddleFooterTransforms.ts
@@ -9,6 +9,7 @@ const MAX_TOOLBAR_WIDTH = 32 * REM;
 const MAX_MESSAGES_LIST_WIDTH = 45.5 * REM;
 const SIDE_COLUMN_MAX_WIDTH = 26.5 * REM;
 const MIN_LEFT_COLUMN_WIDTH = 18 * REM;
+const UNPIN_BUTTON_WIDTH = 16.125 * REM;
 
 export default function calculateMiddleFooterTransforms(windowWidth: number, canPost?: boolean) {
   const sidePadding = windowWidth <= MOBILE_SCREEN_MAX_WIDTH
@@ -28,6 +29,8 @@ export default function calculateMiddleFooterTransforms(windowWidth: number, can
 
   const composerHiddenScale = toolbarWidth / composerWidth;
   const toolbarHiddenScale = composerWidth / toolbarWidth;
+  const unpinHiddenScale = toolbarWidth / UNPIN_BUTTON_WIDTH;
+  const toolbarForUnpinHiddenScale = UNPIN_BUTTON_WIDTH / toolbarWidth;
 
   const composerTranslateX = canPost
     ? (sendButtonWidth / 2) * toolbarHiddenScale
@@ -42,6 +45,8 @@ export default function calculateMiddleFooterTransforms(windowWidth: number, can
     toolbarHiddenScale,
     composerTranslateX,
     toolbarTranslateX,
+    unpinHiddenScale,
+    toolbarForUnpinHiddenScale,
   };
 }
 
