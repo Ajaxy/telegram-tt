bedd3d2119dce12e26e37a070338bc18aeff8611
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-04-29 13:10:42 +0300

    [Build]

--- dist/8.3b501d6b87ba28c80c1e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{303:function(n,w,a){"use strict";a.r(w);var o=a(297),p=a.n(o);a(301);w.default=p.a}}]);
-//# sourceMappingURL=8.3b501d6b87ba28c80c1e.js.map
