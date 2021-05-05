987dfacaf518d4e2ef642a65c0931b422065974e
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-05-06 01:47:36 +0300

    App Inactive: Support disconnecting when opening in another tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
 import { FC, useEffect } from './lib/teact/teact';
 import React, { withGlobal } from './lib/teact/teactn';
 
-import { GlobalState } from './global/types';
+import { GlobalActions, GlobalState } from './global/types';
 
+import { INACTIVE_MARKER, PAGE_TITLE } from './config';
 import { pick } from './util/iteratees';
 import { updateSizes } from './util/windowSize';
+import { addActiveTabChangeListener } from './util/activeTabMonitor';
+import useFlag from './hooks/useFlag';
 
 import Auth from './components/auth/Auth';
 import UiLoader from './components/common/UiLoader';
 import Main from './components/main/Main.async';
+import AppInactive from './components/main/AppInactive';
 // import Test from './components/test/TestNoRedundancy';
 
 type StateProps = Pick<GlobalState, 'authState' | 'authIsSessionRemembered'>;
+type DispatchProps = Pick<GlobalActions, 'disconnect'>;
 
-const App: FC<StateProps> = ({ authState, authIsSessionRemembered }) => {
+const App: FC<StateProps & DispatchProps> = ({ authState, authIsSessionRemembered, disconnect }) => {
+  const [isInactive, markInactive] = useFlag(false);
+
   useEffect(() => {
     updateSizes();
-  }, []);
+    addActiveTabChangeListener(() => {
+      disconnect();
+      document.title = `${PAGE_TITLE}${INACTIVE_MARKER}`;
+
+      markInactive();
+    });
+  }, [disconnect, markInactive]);
 
   // return <Test />;
+
+  if (isInactive) {
+    return <AppInactive />;
+  }
 
   if (authState) {
     switch (authState) {
@@ -49,4 +66,5 @@ function renderMain() {
 
 export default withGlobal(
   (global): StateProps => pick(global, ['authState', 'authIsSessionRemembered']),
+  (setGlobal, actions): DispatchProps => pick(actions, ['disconnect']),
 )(App);
