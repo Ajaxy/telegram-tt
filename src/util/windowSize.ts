ccf72732d58b7255b0efe2a016f6216f639a431d
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-05-06 05:11:27 +0300

    Revert "Refresh app when switching screen format or orientation"
    This reverts commit baffa925d88f5639c7125ada338063d90f52ad04.

--- src/util/windowSize.ts
+++ src/util/windowSize.ts
@@ -1,30 +1,11 @@
 import { throttle } from './schedulers';
-import {
-  MOBILE_SCREEN_LANDSCAPE_MAX_HEIGHT,
-  MOBILE_SCREEN_LANDSCAPE_MAX_WIDTH,
-  MOBILE_SCREEN_MAX_WIDTH,
-} from '../config';
-import { IS_MOBILE_SCREEN } from './environment';
 
 type IDimensions = {
   width: number;
   height: number;
 };
 
-const IS_LANDSCAPE = IS_MOBILE_SCREEN && isLandscape();
-
 let windowSize = updateSizes();
-
-const handleResize = throttle(() => {
-  windowSize = updateSizes();
-
-  if ((isMobileScreen() !== IS_MOBILE_SCREEN) || (IS_MOBILE_SCREEN && IS_LANDSCAPE !== !isLandscape())) {
-    window.location.reload();
-  }
-}, 250, true);
-
-window.addEventListener('resize', handleResize);
-window.addEventListener('orientationchange', handleResize);
 
 export function updateSizes(): IDimensions {
   const vh = window.innerHeight * 0.01;
@@ -37,15 +18,12 @@ export function updateSizes(): IDimensions {
   };
 }
 
-function isMobileScreen() {
-  return windowSize.width <= MOBILE_SCREEN_MAX_WIDTH || (
-    windowSize.width <= MOBILE_SCREEN_LANDSCAPE_MAX_WIDTH && windowSize.height <= MOBILE_SCREEN_LANDSCAPE_MAX_HEIGHT
-  );
-}
+const handleResize = throttle(() => {
+  windowSize = updateSizes();
+}, 250, true);
 
-function isLandscape() {
-  return window.matchMedia('(orientation: landscape)').matches;
-}
+window.addEventListener('resize', handleResize);
+window.addEventListener('orientationchange', handleResize);
 
 export default {
   get: () => windowSize,
