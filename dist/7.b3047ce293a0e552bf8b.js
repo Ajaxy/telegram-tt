278b94ca49404c9f8782983e25264ea7e0d62fc9
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-05-09 04:08:19 +0300

    [Build]

--- dist/7.b3047ce293a0e552bf8b.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{304:function(n,w,a){"use strict";a.r(w);var o=a(299),p=a.n(o);a(303);w.default=p.a}}]);
-//# sourceMappingURL=7.b3047ce293a0e552bf8b.js.map
