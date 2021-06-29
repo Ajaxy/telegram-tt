3c35b824849fee89e3e50b5092cf8a8b3808d73e
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-06-30 00:22:59 +0300

    Rename Telegram T -> Telegram WebZ; Add description for web page previews

--- src/components/payment/PaymentModal.tsx
+++ src/components/payment/PaymentModal.tsx
@@ -301,7 +301,7 @@ const Invoice: FC<OwnProps & StateProps & GlobalStateProps & DispatchProps> = ({
         onClose={onClose}
       >
         <p>
-          Sorry, Telegram T doesn&apos;t support payments with this provider yet. <br />
+          Sorry, Telegram WebZ doesn&apos;t support payments with this provider yet. <br />
           Please use one of our mobile apps to do this.
         </p>
         <Button
