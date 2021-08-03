d8fdf881da14116e245fc587b756772a9b450ba4
Alexander Zinchuk <alx.zinchuk@gmail.com>
2021-08-03 19:02:59 +0300

    [Teact] [Dev] Add more debug tools

--- src/hooks/useLayoutEffectWithPrevDeps.ts
+++ src/hooks/useLayoutEffectWithPrevDeps.ts
@@ -1,11 +1,11 @@
 import { useLayoutEffect } from '../lib/teact/teact';
 import usePrevious from './usePrevious';
 
-export default <T extends any[], PT = T>(cb: (args: PT) => void, dependencies: T) => {
+export default <T extends any[], PT = T>(cb: (args: PT) => void, dependencies: T, debugKey?: string) => {
   const prevDeps = usePrevious<T>(dependencies);
   return useLayoutEffect(() => {
     // @ts-ignore (workaround for "could be instantiated with a different subtype" issue)
-    cb(prevDeps || []);
+    return cb(prevDeps || []);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, dependencies);
+  }, dependencies, debugKey);
 };
