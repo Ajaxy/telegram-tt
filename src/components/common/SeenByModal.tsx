24e1040f8a298486989251ef2c8f98f156a569da
StuckDuck7710 <stuck@stuckduck.it>
2022-01-05 08:17:07 +0100

    Fixed language string in `SeenByModal`

--- src/components/common/SeenByModal.tsx
+++ src/components/common/SeenByModal.tsx
@@ -46,7 +46,7 @@ const SeenByModal: FC<OwnProps & StateProps> = ({
       isOpen={isOpen}
       onClose={closeSeenByModal}
       className="narrow"
-      title="Which users read the message"
+      title={lang("MessageSeen")}
     >
       <div dir={lang.isRtl ? 'rtl' : undefined}>
         {renderingMemberIds && renderingMemberIds.map((userId) => (
