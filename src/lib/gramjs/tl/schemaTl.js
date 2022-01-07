f717443808e51ae43609dfac75202f5051a31dc9
Alexander Zinchuk <alx.zinchuk@gmail.com>
2022-01-07 11:19:48 +0100

    GramJs: Fix auto-generated schemas

--- src/lib/gramjs/tl/schemaTl.js
+++ src/lib/gramjs/tl/schemaTl.js
@@ -1,5 +1,8 @@
 module.exports = `resPQ#05162463 nonce:int128 server_nonce:int128 pq:string server_public_key_fingerprints:Vector<long> = ResPQ;
 p_q_inner_data#83c95aec pq:string p:string q:string nonce:int128 server_nonce:int128 new_nonce:int256 = P_Q_inner_data;
+p_q_inner_data_dc#a9f55f95 pq:string p:string q:string nonce:int128 server_nonce:int128 new_nonce:int256 dc:int = P_Q_inner_data;
+p_q_inner_data_temp#3c6a84d4 pq:string p:string q:string nonce:int128 server_nonce:int128 new_nonce:int256 expires_in:int = P_Q_inner_data;
+p_q_inner_data_temp_dc#56fddf88 pq:string p:string q:string nonce:int128 server_nonce:int128 new_nonce:int256 dc:int expires_in:int = P_Q_inner_data;
 server_DH_params_fail#79cb045d nonce:int128 server_nonce:int128 new_nonce_hash:int128 = Server_DH_Params;
 server_DH_params_ok#d0e8075c nonce:int128 server_nonce:int128 encrypted_answer:string = Server_DH_Params;
 server_DH_inner_data#b5890dba nonce:int128 server_nonce:int128 g:int dh_prime:string g_a:string server_time:int = Server_DH_inner_data;
@@ -28,12 +31,27 @@ msg_detailed_info#276d3ec6 msg_id:long answer_msg_id:long bytes:int status:int =
 msg_new_detailed_info#809db6df answer_msg_id:long bytes:int status:int = MsgDetailedInfo;
 msg_resend_req#7d861a08 msg_ids:Vector<long> = MsgResendReq;
 rpc_error#2144ca19 error_code:int error_message:string = RpcError;
+rpc_answer_unknown#5e2ad36e = RpcDropAnswer;
+rpc_answer_dropped_running#cd78e586 = RpcDropAnswer;
+rpc_answer_dropped#a43ad8b7 msg_id:long seq_no:int bytes:int = RpcDropAnswer;
 future_salt#0949d9dc valid_since:int valid_until:int salt:long = FutureSalt;
 future_salts#ae500895 req_msg_id:long now:int salts:vector<future_salt> = FutureSalts;
 pong#347773c5 msg_id:long ping_id:long = Pong;
 destroy_session_ok#e22045fc session_id:long = DestroySessionRes;
 destroy_session_none#62d350c9 session_id:long = DestroySessionRes;
 new_session_created#9ec20908 first_msg_id:long unique_id:long server_salt:long = NewSession;
+http_wait#9299359f max_delay:int wait_after:int max_wait:int = HttpWait;
+ipPort#d433ad73 ipv4:int port:int = IpPort;
+ipPortSecret#37982646 ipv4:int port:int secret:bytes = IpPort;
+accessPointRule#4679b65f phone_prefix_rules:string dc_id:int ips:vector<IpPort> = AccessPointRule;
+help.configSimple#5a592a6c date:int expires:int rules:vector<AccessPointRule> = help.ConfigSimple;
+tlsClientHello blocks:vector<TlsBlock> = TlsClientHello;
+tlsBlockString data:string = TlsBlock;
+tlsBlockRandom length:int = TlsBlock;
+tlsBlockZero length:int = TlsBlock;
+tlsBlockDomain = TlsBlock;
+tlsBlockGrease seed:int = TlsBlock;
+tlsBlockScope entries:Vector<TlsBlock> = TlsBlock;
 ---functions---
 ping#7abe77ec ping_id:long = Pong;
 ping_delay_disconnect#f3427b8c ping_id:long disconnect_delay:int = Pong;`;
