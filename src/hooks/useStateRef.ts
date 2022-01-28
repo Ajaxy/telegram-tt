479c4764eb49b58ea64a18f8b964df5cb25c2a0b
Alexander Zinchuk <alx.zinchuk@gmail.com>
2022-01-28 02:12:10 +0100

    Composer: Fix input tooltips

--- src/hooks/useStateRef.ts
+++ src/hooks/useStateRef.ts
@@ -1,11 +1,13 @@
-import { useEffect, useRef } from '../lib/teact/teact';
+import { useRef } from '../lib/teact/teact';
+
+import useOnChange from './useOnChange';
 
 // Allows to use state value as "silent" dependency in hooks (not causing updates).
 // Useful for state values that update frequently (such as controlled input value).
 export function useStateRef<T>(value: T) {
   const ref = useRef<T>(value);
 
-  useEffect(() => {
+  useOnChange(() => {
     ref.current = value;
   }, [value]);
 
