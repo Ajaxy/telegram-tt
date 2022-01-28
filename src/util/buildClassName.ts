c12b58a9f1618084a889d39407db41742281ab5b
Alexander Zinchuk <alx.zinchuk@gmail.com>
2022-01-28 02:11:05 +0100

    [Refactoring] Spoiler: Use BEM

--- src/util/buildClassName.ts
+++ src/util/buildClassName.ts
@@ -1,5 +1,19 @@
 type Parts = (string | false | undefined)[];
 
-export default (...parts: Parts) => {
+export default function buildClassName(...parts: Parts) {
   return parts.filter(Boolean).join(' ');
-};
+}
+
+export function createClassNameBuilder(componentName: string) {
+  return (elementName: string, ...modifiers: Parts) => {
+    const baseName = elementName === '&' ? componentName : `${componentName}__${elementName}`;
+
+    return modifiers.reduce((acc, modifier) => {
+      if (modifier) {
+        acc.push(`${baseName}--${modifier}`);
+      }
+
+      return acc;
+    }, [baseName]).join(' ');
+  };
+}
