59ea096c7b0fa058a04cbf189f24b6d8632abb85
Alexander Zinchuk <alx.zinchuk@gmail.com>
2022-03-09 01:02:57 +0100

    Reactions: Skip notifications about self reactions (#1753)

--- src/modules/helpers/reactions.ts
+++ src/modules/helpers/reactions.ts
@@ -4,9 +4,11 @@ export function getMessageRecentReaction(message: Partial<ApiMessage>) {
   return message.isOutgoing ? message.reactions?.recentReactions?.[0] : undefined;
 }
 
-export function checkIfReactionAdded(oldReactions?: ApiReactions, newReactions?: ApiReactions) {
+export function checkIfReactionAdded(oldReactions?: ApiReactions, newReactions?: ApiReactions, currentUserId?: string) {
   if (!oldReactions || !oldReactions.recentReactions) return true;
   if (!newReactions || !newReactions.recentReactions) return false;
+  // Skip reactions from yourself
+  if (newReactions.recentReactions.every(reaction => reaction.userId === currentUserId)) return false;
   const oldReactionsMap = oldReactions.results.reduce<Record<string, number>>((acc, reaction) => {
     acc[reaction.reaction] = reaction.count;
     return acc;
