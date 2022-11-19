7aca07145d49c314220a63b223aeac6dd00c76b1
Alexander Zinchuk <alx.zinchuk@gmail.com>
2022-11-19 22:48:33 +0300

    PWA: Support system sharing menu (follow-up 2) (#2150)

--- src/serviceWorker/share.ts
+++ src/serviceWorker/share.ts
@@ -25,7 +25,7 @@ export async function respondForShare(e: FetchEvent) {
     }
   }
 
-  return Response.redirect('..');
+  return Response.redirect('.');
 }
 
 export function handleClientMessage(e: ExtendableMessageEvent) {
