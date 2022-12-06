20549cb6aaf99271bcf91dbcb750dfe24fe486ec
Alexander Zinchuk <alx.zinchuk@gmail.com>
2022-12-06 13:29:40 +0100

    Login: Fix `pt-br` suggestion translation (#2171)

--- src/components/auth/helpers/getSuggestedLanguage.ts
+++ src/components/auth/helpers/getSuggestedLanguage.ts
@@ -1,7 +1,7 @@
 import type { LangCode } from '../../../types';
 
 export function getSuggestedLanguage() {
-  let suggestedLanguage = navigator.language;
+  let suggestedLanguage = navigator.language.toLowerCase();
 
   if (suggestedLanguage && suggestedLanguage !== 'pt-br') {
     suggestedLanguage = suggestedLanguage.substr(0, 2);
