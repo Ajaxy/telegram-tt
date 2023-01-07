559f83af7c887f6e8d59b8de1a9c9414f4e6d7dc
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-01-07 19:08:57 +0100

    [Refactoring] Fix typings

--- src/util/requestActionTimeout.ts
+++ src/util/requestActionTimeout.ts
@@ -1,11 +1,11 @@
 import { getActions } from '../global';
 import type { GlobalActions } from '../global/types';
 
-const callbacks = new Map<string, NodeJS.Timeout>();
+const callbacks = new Map<string, number>();
 
 export default function requestActionTimeout(action: keyof GlobalActions, timeout: number) {
   clearTimeout(callbacks.get(action));
-  const timerId = setTimeout(() => {
+  const timerId = window.setTimeout(() => {
     getActions()[action]();
   }, timeout);
   callbacks.set(action, timerId);
