3d10708a56c1b94b10c2a824ce17cf6dacbaf69e
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-02-17 02:31:46 +0100

    General: Fix page title reset in Chromium (#2580)

--- src/util/updatePageTitle.ts
+++ src/util/updatePageTitle.ts
@@ -1,7 +1,14 @@
+import { debounce } from './schedulers';
+
+const UPDATE_DEBOUNCE_MS = 200;
+
 // For some reason setting `document.title` to the same value
 // causes increment of Chrome Dev Tools > Performance Monitor > DOM Nodes counter
-export default function updatePageTitle(nextTitle: string) {
+function updatePageTitle(nextTitle: string) {
   if (document.title !== nextTitle) {
     document.title = nextTitle;
   }
 }
+
+// Synchronous page title update has conflicts with History API in Chrome
+export default debounce(updatePageTitle, UPDATE_DEBOUNCE_MS, false);
