2c8b22b964f706ca9f9572650d7bcccc1d9f933d
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-03-30 18:25:04 -0500

    Composer: Preserve caret position on mention insert (#2849)

--- src/util/selection.ts
+++ src/util/selection.ts
@@ -48,3 +48,47 @@ export function getHtmlBeforeSelection(container?: HTMLElement, useCommonAncesto
 
   return extractorEl.innerHTML;
 }
+
+// https://stackoverflow.com/a/3976125
+export function getCaretPosition(element: HTMLElement) {
+  let caretPosition = 0;
+  const selection = window.getSelection();
+  if (!selection || selection.rangeCount === 0) {
+    return caretPosition;
+  }
+
+  const range = selection.getRangeAt(0);
+  const caretRange = range.cloneRange();
+  caretRange.selectNodeContents(element);
+  caretRange.setEnd(range.endContainer, range.endOffset);
+  caretPosition = caretRange.toString().length;
+
+  return caretPosition;
+}
+
+// https://stackoverflow.com/a/36953852
+export function setCaretPosition(element: Node, position: number) {
+  for (const node of element.childNodes) {
+    if (node.nodeType === Node.TEXT_NODE) {
+      if ((node as Text).length >= position) {
+        const range = document.createRange();
+        const selection = window.getSelection()!;
+        range.setStart(node, position);
+        range.collapse(true);
+        selection.removeAllRanges();
+        selection.addRange(range);
+
+        return -1;
+      } else {
+        position -= 'length' in node ? node.length as number : 0;
+      }
+    } else {
+      position = setCaretPosition(node, position);
+      if (position === -1) {
+        return -1;
+      }
+    }
+  }
+
+  return position;
+}
