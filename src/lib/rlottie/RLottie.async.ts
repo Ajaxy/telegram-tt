7e53d0da57e9b73bc73e87168fcf3df287d1c257
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-04-25 17:24:51 +0400

    RLottie: Fix synchronous bundling (#3071)

---
+++ src/lib/rlottie/RLottie.async.ts
@@ -0,0 +1,22 @@
+type RLottieClass = typeof import('./RLottie').default;
+
+let promise: Promise<RLottieClass>;
+let RLottie: RLottieClass;
+
+// Time for the main interface to completely load
+const LOTTIE_LOAD_DELAY = 3000;
+
+export async function ensureRLottie() {
+  if (!promise) {
+    promise = import('./RLottie').then((module) => module.default);
+    RLottie = await promise;
+  }
+
+  return promise;
+}
+
+export function getRLottie() {
+  return RLottie;
+}
+
+setTimeout(ensureRLottie, LOTTIE_LOAD_DELAY);
