4a53e572843f3c8c7cfb805ca7703e297c9394c6
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-04-26 21:15:22 +0400

    Manage Topic: Fix default icon (#3083)

--- src/components/common/TopicIcon.tsx
+++ src/components/common/TopicIcon.tsx
@@ -18,6 +18,7 @@ type OwnProps = {
 };
 
 const LOOP_LIMIT = 2;
+const DEFAULT_ICON_ID = '0';
 
 const TopicIcon: FC<OwnProps> = ({
   topic,
@@ -28,7 +29,7 @@ const TopicIcon: FC<OwnProps> = ({
   observeIntersection,
   onClick,
 }) => {
-  if (topic.iconEmojiId) {
+  if (topic.iconEmojiId && topic.iconEmojiId !== DEFAULT_ICON_ID) {
     return (
       <CustomEmoji
         documentId={topic.iconEmojiId}
