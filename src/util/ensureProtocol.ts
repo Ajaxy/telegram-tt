b8f11550018a807f5ead9daed67a62f5c2a5f41e
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-06-20 20:55:18 +0200

    Link: Fix protocol verification (#3417)

--- src/util/ensureProtocol.ts
+++ src/util/ensureProtocol.ts
@@ -5,5 +5,15 @@ export function ensureProtocol(url?: string) {
 
   // HTTP was chosen by default as a fix for https://bugs.telegram.org/c/10712.
   // It is also the default protocol in the official TDesktop client.
-  return url.includes('://') ? url : `http://${url}`;
+  try {
+    const parsedUrl = new URL(url);
+    // eslint-disable-next-line no-script-url
+    if (parsedUrl.protocol === 'javascript:') {
+      return `http://${url}`;
+    }
+
+    return url;
+  } catch (err) {
+    return `http://${url}`;
+  }
 }
