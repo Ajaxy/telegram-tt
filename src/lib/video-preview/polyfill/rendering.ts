faef077676bbe19924395028968d63c36210051a
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-07-20 15:58:51 +0200

    Media Viewer: Fix video preview errors in Firefox (#3602)

--- src/lib/video-preview/polyfill/rendering.ts
+++ src/lib/video-preview/polyfill/rendering.ts
@@ -178,7 +178,7 @@ export function createImageBitmap(
     for (let i = 0; i < frame.data.length; i++) {
       const plane = frame.data[i];
       for (let y = 0; y < plane.length; y++) {
-        const row = plane[y];
+        const row = plane[y].subarray(0, image.codedWidth * 4);
         frameData.data.set(row, idx);
         idx += row.length;
       }
