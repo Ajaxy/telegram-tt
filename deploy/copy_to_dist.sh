a739bda3298e7b65d62bf4ac1a611bb86eddf4c1
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-09-30 01:53:47 +0200

    Bump dependencies

--- deploy/copy_to_dist.sh
+++ deploy/copy_to_dist.sh
@@ -4,8 +4,6 @@ cp -R ./public/* ${1:-"dist"}
 
 cp ./src/lib/rlottie/rlottie-wasm.wasm ${1:-"dist"}
 
-cp ./src/lib/webp/webp_wasm.wasm ${1:-"dist"}
-
 cp ./node_modules/opus-recorder/dist/decoderWorker.min.wasm ${1:-"dist"}
 
 cp -R ./node_modules/emoji-data-ios/img-apple-64 ${1:-"dist"}
