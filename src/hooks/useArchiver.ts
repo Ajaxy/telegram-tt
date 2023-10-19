f1bd591c13394578a5e4d12126052529f04aa76c
iower <iower@tutanota.com>
2023-10-19 12:56:39 +0500

    Improve algo

--- src/hooks/useArchiver.ts
+++ src/hooks/useArchiver.ts
@@ -4,42 +4,50 @@ Inspired by https://github.com/rebryk/supertelega
 See original code here: https://github.com/rebryk/supertelega/blob/master/archive.py
 */
 
-import { useState } from '../lib/teact/teact';
-import { getGlobal } from '../global';
+// import { useState } from '../lib/teact/teact';
+import { getActions, getGlobal } from '../global';
 
 import type { ApiChat } from '../api/types';
 
 import useInterval from './useInterval';
 
 const UPDATE_TIME_SEC = 5;
-const MESSAGE_DISPLAY_TIME_SEC = 6;
+const MESSAGE_DISPLAY_TIME_SEC = 10;
 
 export default function useArchiver() {
-  const [isBusy, setIsBusy] = useState(false);
-  const global = getGlobal();
+  // const [isBusy, setIsBusy] = useState(false);
+  const { toggleChatArchived } = getActions();
 
   const chatsToArchive: { [key: string]: Date } = {};
 
   const shouldArchive = (chat: ApiChat) => {
-    return chat.isMuted || !(
+    return chat && (chat.isMuted || !(
       chat.hasUnreadMark
       || chat.unreadCount
       || chat.unreadMentionsCount
       || chat.unreadReactionsCount
-    );
+    ));
   };
 
   const add = (chat: ApiChat) => {
-    if (!chatsToArchive[chat.id]) {
+    if (chat && chat.id && !chatsToArchive[chat.id]) {
+      // eslint-disable-next-line no-console
+      console.log('archiver | add chat', chat.id);
       chatsToArchive[chat.id] = new Date();
     }
   };
 
   const remove = (chat: ApiChat) => {
-    delete chatsToArchive[chat.id];
+    if (chat && chat.id && chatsToArchive[chat.id]) {
+      // eslint-disable-next-line no-console
+      console.log('archiver | add chat', chat.id);
+      delete chatsToArchive[chat.id];
+    }
   };
 
   const update = () => {
+    // eslint-disable-next-line no-console
+    console.log('archiver | update');
     const now = new Date();
     const idsToArchive = [];
     for (const [chatId, date] of Object.entries(chatsToArchive)) {
@@ -48,42 +56,39 @@ export default function useArchiver() {
         idsToArchive.push(chatId);
       }
     }
-    for (const chatId of idsToArchive) {
-      idsToArchive.pop();
+    // eslint-disable-next-line no-console
+    console.log('archiver | update idsToArchive', idsToArchive.length);
+    for (const id of idsToArchive.slice(0, 5)) {
+      toggleChatArchived({ id });
       // eslint-disable-next-line no-console
-      console.log('Archive', chatId);
+      console.log('archiver | toggleChatArchived', id);
     }
   };
 
   const process = () => {
     // eslint-disable-next-line no-console
-    console.log('Archive process');
-    setIsBusy(true);
+    console.log('archiver | process');
+    // setIsBusy(true);
+    const global = getGlobal();
     const notArchivedChatsIds = global.chats.listIds.active;
     if (notArchivedChatsIds) {
-      for (let i = 0; i < 10; i++) {
-        const chatId = notArchivedChatsIds[i];
+      for (const chatId of notArchivedChatsIds) {
         const chatsById = global.chats.byId;
         const chat = chatsById[chatId];
         if (shouldArchive(chat)) {
           add(chat);
-          // eslint-disable-next-line no-console
-          console.log('Archive Add chat', chat.id);
         } else {
           remove(chat);
         }
-        update();
-
-        // eslint-disable-next-line no-console
-        // console.log('Archiver', chatId, chat.hasUnreadMark);
       }
+      update();
     }
-    setIsBusy(false);
+    // setIsBusy(false);
   };
 
   useInterval(() => {
-    if (!isBusy) {
-      process();
-    }
+    // if (!isBusy) {
+    process();
+    // }
   }, UPDATE_TIME_SEC * 1000);
 }
