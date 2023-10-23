6a38ab2a0e0fd371cece225eaeb059cbd14d8553
iower <iower@tutanota.com>
2023-10-23 12:51:30 +0500

    More reliable classes

--- src/components/left/MuteChatModal.tsx
+++ src/components/left/MuteChatModal.tsx
@@ -83,8 +83,8 @@ const MuteChatModal: FC<OwnProps> = ({
       <div className="dialog-buttons">
         <Button color="primary" className="confirm-dialog-button" isText onClick={handleSubmit}>
           {lang('Common.Done')}
-          <div className="frame">
-            <div className="text-wrapper">⏎</div>
+          <div className="hotkey-frame">
+            <div className="hotkey-text">⏎</div>
           </div>
         </Button>
         <Button className="confirm-dialog-button" isText onClick={onClose}>{lang('Cancel')}</Button>
