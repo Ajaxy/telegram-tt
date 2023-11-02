1c5e9db2a09a52edd309f1712dc650d6b12c7bc9
iower <iower@tutanota.com>
2023-11-02 17:21:52 +0500

    Ignore & delete: dist/*, public/build-stats.json, public/statoscope-report.html

--- dist/3848.767ff8e5d12d8164f976.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktelegram_t=self.webpackChunktelegram_t||[]).push([[3848],{69609:e=>{e.exports=function(e){return{name:"Node REPL",contains:[{className:"meta.prompt",starts:{end:/ |$/,starts:{end:"$",subLanguage:"javascript"}},variants:[{begin:/^>(?=[ ]|$)/},{begin:/^\.\.\.(?=[ ]|$)/}]}]}}}}]);
-//# sourceMappingURL=3848.767ff8e5d12d8164f976.js.map
