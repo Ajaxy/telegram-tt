9ab89be93ade52c0c54369391b34ccfcb0b9b9df
Alexander Zinchuk <alx.zinchuk@gmail.com>
2023-11-06 01:43:00 +0400

    Electron: Sanitize application version during update check (#3933)

--- src/util/getIsAppUpdateNeeded.ts
+++ src/util/getIsAppUpdateNeeded.ts
@@ -1,5 +1,7 @@
 const APP_VERSION_REGEX = /^\d+\.\d+(\.\d+)?$/;
 
 export default function getIsAppUpdateNeeded(remoteVersion: string, appVersion: string) {
-  return APP_VERSION_REGEX.test(remoteVersion) && remoteVersion !== appVersion;
+  const sanitizedRemoteVersion = remoteVersion.trim();
+
+  return APP_VERSION_REGEX.test(sanitizedRemoteVersion) && sanitizedRemoteVersion !== appVersion;
 }
