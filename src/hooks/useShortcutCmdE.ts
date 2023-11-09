920f5e7cc20d180b75512214863ab1fe2c038a90
iower <iower@tutanota.com>
2023-11-09 16:57:46 +0500

    Improve useShortcutCmdE

--- src/hooks/useShortcutCmdE.ts
+++ src/hooks/useShortcutCmdE.ts
@@ -1,31 +1,36 @@
 import { useCallback, useEffect } from '../lib/teact/teact';
 import { getActions, getGlobal } from '../global';
 
-import { selectCurrentChat } from '../global/selectors';
+import { selectCurrentChat, selectTabState } from '../global/selectors';
 import { IS_MAC_OS } from '../util/windowEnvironment';
 import { useJune } from './useJune';
 
 function useShortcutCmdE() {
   const { openChat, toggleChatArchived, closeForumPanel } = getActions();
-  const { track } = useJune();
 
-  const global = getGlobal();
-  const currentChat = selectCurrentChat(global);
-  const chatId = currentChat?.id;
+  const { track } = useJune();
 
   const handleKeyDown = useCallback((e: KeyboardEvent) => {
     if (((IS_MAC_OS && e.metaKey) || (!IS_MAC_OS && e.ctrlKey)) && e.code === 'KeyE') {
       e.preventDefault();
-      if (chatId) {
-        toggleChatArchived({ id: chatId });
+
+      const global = getGlobal();
+      const currentChatId = selectCurrentChat(global)?.id;
+      const forumPanelChatId = selectTabState(global).forumPanelChatId;
+
+      const togglingChatId = currentChatId || forumPanelChatId;
+      if (togglingChatId) {
+        toggleChatArchived({ id: togglingChatId });
         openChat({ id: undefined });
-        closeForumPanel();
+        if (togglingChatId === forumPanelChatId) {
+          closeForumPanel();
+        }
         if (track) {
           track('toggleChatArchived');
         }
       }
     }
-  }, [chatId, openChat, closeForumPanel, track]);
+  }, [openChat, closeForumPanel, track]);
 
   useEffect(() => {
     document.addEventListener('keydown', handleKeyDown);
