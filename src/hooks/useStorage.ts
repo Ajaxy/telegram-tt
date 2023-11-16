d266ae3f90b9e0303576388b7bbe7283b294a761
iower <iower@tutanota.com>
2023-11-16 21:52:28 +0500

    useStorage/useLocalStorage, initial|restore

--- src/hooks/useStorage.ts
+++ src/hooks/useStorage.ts
@@ -1,14 +1,51 @@
-import { useState } from '../lib/teact/teact';
+import { useEffect, useState } from '../lib/teact/teact';
 
 export function useStorage() {
-  const [isArchiverEnabled, setStateIsArchiverEnabled] = useState(
-    !!JSON.parse(String(localStorage.getItem('ulu_is_autoarchiver_enabled'))),
-  );
+  const [isArchiverEnabled, setIsArchiverEnabled] = useLocalStorage<boolean>('ulu_is_autoarchiver_enabled', false);
 
-  const setIsArchiverEnabled = (value: boolean) => {
-    localStorage.setItem('ulu_is_autoarchiver_enabled', JSON.stringify(value));
-    setStateIsArchiverEnabled(value);
+  return {
+    isArchiverEnabled, setIsArchiverEnabled,
   };
+}
 
-  return { isArchiverEnabled, setIsArchiverEnabled };
+const EVENT = 'ULU_UPDATE_STORAGE';
+
+function useLocalStorage<T>(key: string, initValue: T): [value: T, setValue: (val: T) => void] {
+  const [state, setState] = useState<T>((() => {
+    const value = localStorage.getItem(key);
+    // eslint-disable-next-line no-null/no-null
+    if (value !== null && value !== undefined) {
+      return JSON.parse(value);
+    }
+
+    localStorage.setItem(key, JSON.stringify(initValue));
+    window.dispatchEvent(new Event(EVENT));
+    return initValue;
+  })());
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+    window.dispatchEvent(new Event(EVENT));
+  }, [key, state]);
+
+  useEffect(() => {
+    const listenStorageChange = () => {
+      setState(() => {
+        const value = localStorage.getItem(key);
+        // eslint-disable-next-line no-null/no-null
+        if (value !== null && value !== undefined) {
+          return JSON.parse(value);
+        }
+
+        localStorage.setItem(key, JSON.stringify(initValue));
+        window.dispatchEvent(new Event(EVENT));
+        return initValue;
+      });
+    };
+    window.addEventListener(EVENT, listenStorageChange);
+    return () => window.removeEventListener(EVENT, listenStorageChange);
+  // eslint-disable-next-line react-hooks-static-deps/exhaustive-deps
+  }, []);
+
+  return [state, setState];
 }
