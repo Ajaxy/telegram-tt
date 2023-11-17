68e11a87d653e7fecea88fc9913db8824522e3fd
serafim.eth <147140426+ulugmer@users.noreply.github.com>
2023-11-17 16:12:33 +0800

    Update LeftMainHeader.tsx

--- src/components/left/main/LeftMainHeader.tsx
+++ src/components/left/main/LeftMainHeader.tsx
@@ -31,6 +31,7 @@ import useCommands from '../../../hooks/useCommands';
 import useConnectionStatus from '../../../hooks/useConnectionStatus';
 import useElectronDrag from '../../../hooks/useElectronDrag';
 import useFlag from '../../../hooks/useFlag';
+import { useFullscreenStatus } from '../../../hooks/useFullscreen';
 import { useHotkeys } from '../../../hooks/useHotkeys';
 import useLang from '../../../hooks/useLang';
 import useLastCallback from '../../../hooks/useLastCallback';
@@ -211,6 +212,8 @@ const LeftMainHeader: FC<OwnProps & StateProps> = ({
 
   const versionString = IS_BETA ? `${APP_VERSION} Beta (${APP_REVISION})` : (DEBUG ? APP_REVISION : APP_VERSION);
 
+  const isFullscreen = useFullscreenStatus();
+
   // Disable dropdown menu RTL animation for resize
   const {
     shouldDisableDropdownMenuTransitionRef,
@@ -290,7 +293,7 @@ const LeftMainHeader: FC<OwnProps & StateProps> = ({
             {isCurrentUserPremium && <StatusButton />}
           </>
         ) : (
-          <div className={`${styles.profileWrapper} ${IS_ELECTRON ? 'electron-header' : ''}`}>
+          <div className={styles.profileWrapper}>
             <DropdownMenu
               trigger={MainButton}
               footer={`${APP_NAME} ${versionString}`}
@@ -302,6 +305,7 @@ const LeftMainHeader: FC<OwnProps & StateProps> = ({
               )}
               forceOpen={isBotMenuOpen}
               positionX={shouldHideSearch && lang.isRtl ? 'right' : 'left'}
+              transformOriginY={IS_ELECTRON && IS_MAC_OS && !isFullscreen ? 50 : undefined}
               onTransitionEnd={lang.isRtl ? handleDropdownMenuTransitionEnd : undefined}
             >
               <LeftSideMenuItems
@@ -317,7 +321,6 @@ const LeftMainHeader: FC<OwnProps & StateProps> = ({
         ) }
         {hasPasscode && (
           <Button
-            round
             ripple={!isMobile}
             size="tiny"
             color="translucent"
