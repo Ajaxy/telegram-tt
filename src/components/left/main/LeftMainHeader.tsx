302cf8b62d11e8a9eb2ba1044aae9cf102e15e43
serafim.eth <147140426+ulugmer@users.noreply.github.com>
2023-11-17 15:56:35 +0800

    fix

--- src/components/left/main/LeftMainHeader.tsx
+++ src/components/left/main/LeftMainHeader.tsx
@@ -318,6 +318,7 @@ const LeftMainHeader: FC<OwnProps & StateProps> = ({
         ) }
         {hasPasscode && (
           <Button
+            round
             ripple={!isMobile}
             size="tiny"
             color="translucent"
