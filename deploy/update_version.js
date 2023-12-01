0a26204a69a771f46d0853737c977088807d2683
iower <iower@tutanota.com>
2023-12-01 19:26:54 +0500

    Cleaner update_version

--- deploy/update_version.js
+++ deploy/update_version.js
@@ -9,20 +9,17 @@ const VERSION_TXT_PATH = `${ROOT_PATH}/public/version.txt`;
 const ULU_VERSION_PATH = `${ROOT_PATH}/.ulu-version`;
 
 // This patch value is used to override the one from package.json
-// const currentPatch = fs.existsSync(PATCH_VERSION_PATH) ? Number(fs.readFileSync(PATCH_VERSION_PATH, 'utf-8')) : -1;
-// ulu-custom-versioning
-const currentPatch = fs.existsSync(PATCH_VERSION_PATH) ? Number(fs.readFileSync(PATCH_VERSION_PATH, 'utf-8')) : 0;
+const currentPatch = fs.existsSync(PATCH_VERSION_PATH) ? Number(fs.readFileSync(PATCH_VERSION_PATH, 'utf-8')) : /* -1 */ 0; // ulu-custom-versioning
 const packageJsonContent = fs.readFileSync(PACKAGE_JSON_PATH, 'utf-8');
 const currentVersion = JSON.parse(packageJsonContent).version;
 const [major, minor] = currentVersion.split('.');
-
-const uluVersion = fs.existsSync(ULU_VERSION_PATH) ? Number(fs.readFileSync(ULU_VERSION_PATH, 'utf-8')) : 0;
-const newUluVersion = uluVersion + 1;
 
 // const newPatch = currentPatch + 1;
 // const newVersion = [major, minor, newPatch].join('.');
 // ulu-custom-versioning
 const newPatch = currentPatch;
+const uluVersion = fs.existsSync(ULU_VERSION_PATH) ? Number(fs.readFileSync(ULU_VERSION_PATH, 'utf-8')) : 0;
+const newUluVersion = uluVersion + 1;
 const newVersion = [major, minor, newPatch].join('.') + '-' + newUluVersion;
 
 const newPackageJsonContent = packageJsonContent.replace(`"version": "${currentVersion}"`, `"version": "${newVersion}"`);
