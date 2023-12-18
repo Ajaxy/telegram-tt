0a32420cd0c599b698de8662f9742f85ebe661fd
iower <iower@tutanota.com>
2023-12-18 14:50:10 +0500

    Fix up ver

--- deploy/update_version.js
+++ deploy/update_version.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const ROOT_PATH = `${path.dirname(__filename)}/..`;
 const PATCH_VERSION_PATH = `${ROOT_PATH}/.patch-version`;
 const PACKAGE_JSON_PATH = `${ROOT_PATH}/package.json`;
+const PACKAGE_LOCK_JSON_PATH = `${ROOT_PATH}/package_lock.json`;
 const VERSION_TXT_PATH = `${ROOT_PATH}/public/version.txt`;
 
 const ULU_VERSION_PATH = `${ROOT_PATH}/.ulu-version`;
@@ -11,6 +12,7 @@ const ULU_VERSION_PATH = `${ROOT_PATH}/.ulu-version`;
 // This patch value is used to override the one from package.json
 const currentPatch = fs.existsSync(PATCH_VERSION_PATH) ? Number(fs.readFileSync(PATCH_VERSION_PATH, 'utf-8')) : /* -1 */ 0; // ulu-custom-versioning
 const packageJsonContent = fs.readFileSync(PACKAGE_JSON_PATH, 'utf-8');
+const packageLockJsonContent = fs.readFileSync(PACKAGE_LOCK_JSON_PATH, 'utf-8');
 const currentVersion = JSON.parse(packageJsonContent).version;
 const [major, minor] = currentVersion.split('.');
 
@@ -23,6 +25,7 @@ const newUluVersion = uluVersion + 1;
 const newVersion = [major, minor, newPatch].join('.') + '-' + newUluVersion;
 
 const newPackageJsonContent = packageJsonContent.replace(`"version": "${currentVersion}"`, `"version": "${newVersion}"`);
+const newPackageLockJsonContent = packageLockJsonContent.replace(`"version": "${currentVersion}"`, `"version": "${newVersion}"`);
 
 console.log(`[update_version] ${currentVersion} -> ${newVersion}\n`);
 
