4900c565e370f421946ec79c57a54db0a0de3534
ulu <147140426+ulugmer@users.noreply.github.com>
2023-12-21 13:37:53 +0800

    Return forceQuit without leaving fullscreen

--- src/electron/window.ts
+++ src/electron/window.ts
@@ -98,24 +98,22 @@ export function createWindow(url?: string) {
     window.webContents.send(ElectronEvent.FULLSCREEN_CHANGE, false);
   });
 
-  window.on('close', (event) => {
+    window.on('close', (event) => {
     const focusedWindow = getCurrentWindow();
+    if (forceQuit.isEnabled) {
+      app.exit(0);
+      return;
+    }
 
     if (focusedWindow && focusedWindow.isFullScreen()) {
       event.preventDefault();
       focusedWindow.once('leave-full-screen', () => {
-        if (forceQuit.isEnabled) {
-          app.exit(0);
-        } else {
-          focusedWindow.close();
-        }
+        focusedWindow.close();
       });
       focusedWindow.setFullScreen(false);
     } else if (IS_MAC_OS || (IS_WINDOWS && tray.isEnabled)) {
-      if (forceQuit.isEnabled) {
-        app.exit(0);
-        forceQuit.disable();
-      } else if (hasExtraWindows()) {
+      // Обычная логика закрытия
+      if (hasExtraWindows()) {
         windows.delete(window);
         windowState.unmanage();
       } else {
