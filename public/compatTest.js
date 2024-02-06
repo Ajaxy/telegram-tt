0ba12d4dd5d0aca8635c2703b197ef8182dc97ba
Alexander Zinchuk <alx.zinchuk@gmail.com>
2024-02-06 16:49:28 +0100

    Drop Waterfox Classic support (#4237)

--- public/compatTest.js
+++ public/compatTest.js
@@ -3,8 +3,13 @@ function compatTest() {
   var hasWebSockets = typeof WebSocket !== 'undefined';
   var hasWebCrypto = window.crypto && typeof window.crypto.subtle !== 'undefined';
   var hasObjectFromEntries = typeof Object.fromEntries !== 'undefined';
+  var hasResizeObserver = typeof window.ResizeObserver !== 'undefined';
+  var hasCssSupports = window.CSS && typeof window.CSS.supports === 'function';
+  var hasIntl = typeof window.Intl !== 'undefined';
+  var hasDisplayNames = hasIntl && typeof Intl.DisplayNames !== 'undefined';
 
-  var isCompatible = hasPromise && hasWebSockets && hasWebCrypto && hasObjectFromEntries;
+  var isCompatible = hasPromise && hasWebSockets && hasWebCrypto && hasObjectFromEntries && hasResizeObserver
+    && hasCssSupports && hasDisplayNames;
 
   if (isCompatible || (window.localStorage && window.localStorage.getItem('tt-ignore-compat'))) {
     window.isCompatTestPassed = true;
@@ -17,6 +22,9 @@ function compatTest() {
     console.warn('WebSocket', hasWebSockets);
     console.warn('WebCrypto', hasWebCrypto);
     console.warn('Object.fromEntries', hasObjectFromEntries);
+    console.warn('ResizeObserver', hasResizeObserver);
+    console.warn('CSS.supports', hasCssSupports);
+    console.warn('Intl.DisplayNames', hasDisplayNames);
   }
 
   document.body.innerHTML = '<iframe src="./unsupported.html" width="100%" height="100%">';
