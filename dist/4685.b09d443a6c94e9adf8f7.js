e9cbad864a9ca562db2a541563d769df197760f5
Koval Yaroslav <yaroslav@koval.page>
2024-03-06 15:40:32 +0200

    Add support Cloudflare Pages

--- dist/4685.b09d443a6c94e9adf8f7.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktelegram_t=self.webpackChunktelegram_t||[]).push([[4685],{8895:e=>{e.exports=function(e){return{name:"Mojolicious",subLanguage:"xml",contains:[{className:"meta",begin:"^__(END|DATA)__$"},{begin:"^\\s*%{1,2}={0,2}",end:"$",subLanguage:"perl"},{begin:"<%{1,2}={0,2}",end:"={0,1}%>",subLanguage:"perl",excludeBegin:!0,excludeEnd:!0}]}}}}]);
-//# sourceMappingURL=4685.b09d443a6c94e9adf8f7.js.map
--- dist/5204.24f538ca3317cade6c01.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktelegram_t=self.webpackChunktelegram_t||[]).push([[5204],{62437:e=>{e.exports=function(e){return{name:"Plain text",aliases:["text","txt"],disableAutodetect:!0}}}}]);
-//# sourceMappingURL=5204.24f538ca3317cade6c01.js.map
