7bf1b2b8783a86acf56bce8790f762cd2c992a5f
Alexander Zinchuk <alx.zinchuk@gmail.com>
2024-03-08 12:48:44 +0100

    Contact: New design for shared contact (#4333)

--- src/components/middle/message/Contact.tsx
+++ src/components/middle/message/Contact.tsx
@@ -4,15 +4,19 @@ import { getActions, withGlobal } from '../../../global';
 
 import type { ApiContact, ApiCountryCode, ApiUser } from '../../../api/types';
 
+import { getCanAddContact, getUserFullName } from '../../../global/helpers';
 import { selectUser } from '../../../global/selectors';
-import buildClassName from '../../../util/buildClassName';
+import { copyTextToClipboard } from '../../../util/clipboard';
 import { formatPhoneNumberWithCode } from '../../../util/phoneNumber';
 
+import useLang from '../../../hooks/useLang';
 import useLastCallback from '../../../hooks/useLastCallback';
 
 import Avatar from '../../common/Avatar';
+import PeerColorWrapper from '../../common/PeerColorWrapper';
+import Button from '../../ui/Button';
 
-import './Contact.scss';
+import styles from './Contact.module.scss';
 
 type OwnProps = {
   contact: ApiContact;
@@ -28,37 +32,80 @@ const UNREGISTERED_CONTACT_ID = '0';
 const Contact: FC<OwnProps & StateProps> = ({
   contact, user, phoneCodeList,
 }) => {
-  const { openChat } = getActions();
+  const lang = useLang();
+  const {
+    openChat, openAddContactDialog, showNotification, openChatWithInfo,
+  } = getActions();
 
   const {
-    firstName,
-    lastName,
     phoneNumber,
     userId,
   } = contact;
   const isRegistered = userId !== UNREGISTERED_CONTACT_ID;
+  const canAddContact = isRegistered && user && getCanAddContact(user);
 
-  const handleClick = useLastCallback(() => {
+  const handleOpenChat = useLastCallback(() => {
     openChat({ id: userId });
   });
 
+  const handleAddContact = useLastCallback(() => {
+    openAddContactDialog({ userId: user?.id });
+  });
+
+  const handleClick = useLastCallback(() => {
+    if (user) {
+      openChatWithInfo({ id: userId });
+    } else {
+      copyTextToClipboard(phoneNumber);
+      showNotification({ message: lang('PhoneCopied') });
+    }
+  });
+
   return (
-    <div
-      className={buildClassName('Contact', isRegistered && 'interactive')}
-      onClick={isRegistered ? handleClick : undefined}
-    >
-      <Avatar
-        size="large"
-        peer={user}
-        text={firstName || lastName}
-      />
-      <div className="contact-info">
-        <div className="contact-name">{firstName} {lastName}</div>
-        <div className="contact-phone">{formatPhoneNumberWithCode(phoneCodeList, phoneNumber)}</div>
+    <PeerColorWrapper peer={user} emojiIconClassName={styles.emojiIconBackground} className={styles.root}>
+      <div className={styles.infoContainer} onClick={handleClick}>
+        <Avatar size="large" peer={user} text={getContactName(contact)} />
+        <div className={styles.info}>
+          <div className={styles.name}>
+            {user ? getUserFullName(user) : getContactName(contact)}
+          </div>
+          <div className={styles.phone}>{formatPhoneNumberWithCode(phoneCodeList, phoneNumber)}</div>
+        </div>
       </div>
-    </div>
+      {isRegistered && (
+        <>
+          <div className={styles.divider} />
+          <div className={styles.buttons}>
+            <Button isText color="translucent" ripple onClick={handleOpenChat} className={styles.button}>
+              {lang('SharedContactMessage')}
+            </Button>
+            {canAddContact && (
+              <Button isText color="translucent" ripple onClick={handleAddContact} className={styles.button}>
+                {lang('SharedContactAdd')}
+              </Button>
+            )}
+          </div>
+        </>
+      )}
+    </PeerColorWrapper>
   );
 };
+
+function getContactName(contact: ApiContact) {
+  if (contact.firstName && contact.lastName) {
+    return `${contact.firstName} ${contact.lastName}`;
+  }
+
+  if (contact.firstName) {
+    return contact.firstName;
+  }
+
+  if (contact.lastName) {
+    return contact.lastName;
+  }
+
+  return '';
+}
 
 export default withGlobal<OwnProps>(
   (global, { contact }): StateProps => {
