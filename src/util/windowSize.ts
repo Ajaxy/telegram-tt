de6d9c36f0f2139dbb98e6dc387bb1f9d69f41f5
zest <mrwangl2018@163.com>
2024-03-22 01:48:10 +0800

    feat: 调整innerHeight取值

--- src/util/windowSize.ts
+++ src/util/windowSize.ts
@@ -4,20 +4,48 @@ import { requestMutation } from '../lib/fasterdom/fasterdom';
 import { throttle } from './schedulers';
 import { IS_IOS } from './windowEnvironment';
 
+const getInnerHeight = () => {
+  let height = window.innerHeight;
+  // @ts-ignore
+  let body = window.document.querySelector('.telegram-a') as Element;
+  let document = window.document;
+  // @ts-ignore
+  // eslint-disable-next-line no-underscore-dangle
+  if (window.__MICRO_APP_ENVIRONMENT__) {
+    // @ts-ignore
+    // eslint-disable-next-line max-len
+    body = (document?.microAppElement?.querySelector?.('.telegram-a')
+      || document.querySelector('.telegram-a')) as Element;
+    // @ts-ignore
+    document = window?.rawDocument || window.document;
+    const bodyRect = body.getBoundingClientRect();
+    height = bodyRect.height;
+  }
+  return height;
+};
+
 const WINDOW_ORIENTATION_CHANGE_THROTTLE_MS = 100;
 const WINDOW_RESIZE_THROTTLE_MS = 250;
 
-let initialHeight = window.innerHeight;
+let initialHeight = getInnerHeight();
 let currentWindowSize = updateSizes();
 
-const handleResize = throttle(() => {
-  currentWindowSize = updateSizes();
-}, WINDOW_RESIZE_THROTTLE_MS, true);
+const handleResize = throttle(
+  () => {
+    currentWindowSize = updateSizes();
+  },
+  WINDOW_RESIZE_THROTTLE_MS,
+  true,
+);
 
-const handleOrientationChange = throttle(() => {
-  initialHeight = window.innerHeight;
-  handleResize();
-}, WINDOW_ORIENTATION_CHANGE_THROTTLE_MS, false);
+const handleOrientationChange = throttle(
+  () => {
+    initialHeight = getInnerHeight();
+    handleResize();
+  },
+  WINDOW_ORIENTATION_CHANGE_THROTTLE_MS,
+  false,
+);
 
 window.addEventListener('orientationchange', handleOrientationChange);
 if (IS_IOS) {
@@ -28,10 +56,34 @@ if (IS_IOS) {
 
 export function updateSizes(): IDimensions {
   let height: number;
+  let dimensions = {
+    width: window.innerWidth,
+    height: getInnerHeight(),
+  };
   if (IS_IOS) {
     height = window.visualViewport!.height + window.visualViewport!.pageTop;
   } else {
-    height = window.innerHeight;
+    height = getInnerHeight();
+  }
+
+  // @ts-ignore
+  let body = window.document.querySelector('.telegram-a') as Element;
+  let document = window.document;
+  // @ts-ignore
+  // eslint-disable-next-line no-underscore-dangle
+  if (window.__MICRO_APP_ENVIRONMENT__) {
+    // @ts-ignore
+    // eslint-disable-next-line max-len
+    body = (document?.microAppElement?.querySelector?.('.telegram-a')
+      || document.querySelector('.telegram-a')) as Element;
+    // @ts-ignore
+    document = window?.rawDocument || window.document;
+    const bodyRect = body.getBoundingClientRect();
+    height = bodyRect.height;
+    dimensions = {
+      height: bodyRect.height,
+      width: bodyRect.width,
+    };
   }
 
   requestMutation(() => {
@@ -39,10 +91,7 @@ export function updateSizes(): IDimensions {
     document.documentElement.style.setProperty('--vh', `${vh}px`);
   });
 
-  return {
-    width: window.innerWidth,
-    height: window.innerHeight,
-  };
+  return dimensions;
 }
 
 const windowSize = {
