d0539397fc016a5751e77f878d20696cdc41c53a
kgchicobava <kgchicobava@gmail.com>
2024-04-12 15:06:29 +0300

    different main frame origin for test

--- src/external/index.ts
+++ src/external/index.ts
@@ -1,85 +1,81 @@
-import { makeRequest } from "../api/gramjs/worker/connector"
-import { getActions, getGlobal } from "../global"
-import { Requester, Responder } from "jsonrpc-iframe"
-import * as CUSTOM from "./custom"
-import { Actions, Custom, Events, Methods } from "./types"
-import { selectChat, selectCurrentMessageList } from "../global/selectors"
+import { makeRequest } from "../api/gramjs/worker/connector";
+import { getActions, getGlobal } from "../global";
+import { Requester, Responder } from "jsonrpc-iframe";
+import * as CUSTOM from "./custom";
+import { Actions, Custom, Events, Methods } from "./types";
+import { selectChat, selectCurrentMessageList } from "../global/selectors";
 
-const MAIN_FRAME_ORIGIN = "https://slise-crm.pages.dev"
+// const MAIN_FRAME_ORIGIN = "https://slise-crm.pages.dev"
+const MAIN_FRAME_ORIGIN = "https://test.slise-crm-app.pages.dev";
 
-let actions = new Responder<Actions>("actions", MAIN_FRAME_ORIGIN)
+let actions = new Responder<Actions>("actions", MAIN_FRAME_ORIGIN);
 
-actions.subscribeUniversal(async (name, args) =>
-{
-	console.log("Received action", name, args)
+actions.subscribeUniversal(async (name, args) => {
+  console.log("Received action", name, args);
 
-	let acts = getActions()
-	let method = acts[name] as (...args: any[]) => any
-	let result = await method(...args)
-	console.log(`${name}(${JSON.stringify(args)}) = `, result)
-	return result
-})
+  let acts = getActions();
+  let method = acts[name] as (...args: any[]) => any;
+  let result = await method(...args);
+  console.log(`${name}(${JSON.stringify(args)}) = `, result);
+  return result;
+});
 
-let clientApi = new Responder<Methods>("methods", MAIN_FRAME_ORIGIN)
+let clientApi = new Responder<Methods>("methods", MAIN_FRAME_ORIGIN);
 
-clientApi.subscribeUniversal((name, args) =>
-{
-	console.log("Received client-api", name)
+clientApi.subscribeUniversal((name, args) => {
+  console.log("Received client-api", name);
 
-	return makeRequest({
-		type: "callMethod",
-		name: name,
-		args: args,
-	})
-})
+  return makeRequest({
+    type: "callMethod",
+    name: name,
+    args: args,
+  });
+});
 
-let custom = new Responder<Custom>("custom", MAIN_FRAME_ORIGIN)
+let custom = new Responder<Custom>("custom", MAIN_FRAME_ORIGIN);
 
-custom.subscribeUniversal((name, args) =>
-{
-	console.log("Received custom", name, args)
+custom.subscribeUniversal((name, args) => {
+  console.log("Received custom", name, args);
 
-	let method = CUSTOM[name] as (...args: any[]) => any
-	return method(...args)
-})
+  let method = CUSTOM[name] as (...args: any[]) => any;
+  return method(...args);
+});
 
-export let events = new Requester<Events>("events", window.parent.window, MAIN_FRAME_ORIGIN)
+export let events = new Requester<Events>(
+  "events",
+  window.parent.window,
+  MAIN_FRAME_ORIGIN
+);
 
-export function __init()
-{
-	let oldChatId: string | undefined
-	let oldAuth = {
-		authed: false,
-		userId: undefined as string | undefined,
-	}
-	const check = () =>
-	{
-		let global = getGlobal()
-		
-		let chatId = selectCurrentMessageList(global)?.chatId
-		if (chatId != oldChatId)
-		{
-			oldChatId = chatId
-			if (chatId)
-			{
-				let chat = selectChat(global, chatId)
-				events.proxy.chatOpened(chatId, chat!)
-			}
-			else
-			{
-				events.proxy.chatClosed()
-			}
-		}
+export function __init() {
+  let oldChatId: string | undefined;
+  let oldAuth = {
+    authed: false,
+    userId: undefined as string | undefined,
+  };
+  const check = () => {
+    let global = getGlobal();
 
-		let auth = CUSTOM.getAuthInfo()
-		if (auth.authed)
-			if (!oldAuth.authed || oldAuth.userId != auth.userId)
-				events.proxy.loggedIn(auth.userId), events.proxy.authStateChanged(auth)
-		
-		if (!auth.authed && oldAuth.authed)
-			events.proxy.loggedOut(), events.proxy.authStateChanged(auth)
+    let chatId = selectCurrentMessageList(global)?.chatId;
+    if (chatId != oldChatId) {
+      oldChatId = chatId;
+      if (chatId) {
+        let chat = selectChat(global, chatId);
+        events.proxy.chatOpened(chatId, chat!);
+      } else {
+        events.proxy.chatClosed();
+      }
+    }
 
-		window.requestAnimationFrame(check)
-	}
-	window.requestAnimationFrame(check)
+    let auth = CUSTOM.getAuthInfo();
+    if (auth.authed)
+      if (!oldAuth.authed || oldAuth.userId != auth.userId)
+        events.proxy.loggedIn(auth.userId), events.proxy.authStateChanged(auth);
+
+    if (!auth.authed && oldAuth.authed)
+      events.proxy.loggedOut(), events.proxy.authStateChanged(auth);
+
+    window.requestAnimationFrame(check);
+  };
+  window.requestAnimationFrame(check);
 }
