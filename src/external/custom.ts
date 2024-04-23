41b7235928d66281622f4a8e73bbbb409db59b19
kgchicobava <kgchicobava@gmail.com>
2024-04-23 16:44:57 +0300

    added peerInfo in chats in folder

--- src/external/custom.ts
+++ src/external/custom.ts
@@ -1,29 +1,36 @@
-import { getGlobal } from "../global"
-import { selectChat, selectChatFolder, selectChatFullInfo, selectChatLastMessage } from "../global/selectors"
+import { getGlobal } from '../global';
 
-export function getChatsInTheFolder(folderId: number)
-{
-	let g = getGlobal()
-	let folder = selectChatFolder(g, folderId)
-	let ids = folder.includedChatIds
-	return ids.map(id => ({
-		id: parseInt(id),
-		chat: selectChat(g, id),
-		fullInfo: selectChatFullInfo(g, id),
-		msg: selectChatLastMessage(g, id, "all"),
-	}))
+import {
+  selectChat,
+  selectChatFolder,
+  selectChatFullInfo,
+  selectChatLastMessage,
+  selectPeer,
+} from '../global/selectors';
+
+export function getChatsInTheFolder(folderId: number) {
+  const g = getGlobal();
+  const folder = selectChatFolder(g, folderId);
+  const ids = folder.includedChatIds;
+  return ids.map((id) => ({
+    id: parseInt(id, 10),
+    chat: selectChat(g, id),
+    fullInfo: selectChatFullInfo(g, id),
+    peerInfo: selectPeer(g, id),
+    msg: selectChatLastMessage(g, id, 'all'),
+  }));
 }
 
-export function getAuthInfo(): { authed: false } | { authed: true, userId: string }
-{
-	let g = getGlobal()
-	let authed = g.authState == "authorizationStateReady"
-	let userId = g.currentUserId
-	if (!authed || !userId)
-		return { authed: false }
+export function getAuthInfo():
+| { authed: false }
+| { authed: true; userId: string } {
+  const g = getGlobal();
+  const authed = g.authState === 'authorizationStateReady';
+  const userId = g.currentUserId;
+  if (!authed || !userId) return { authed: false };
 
-	return {
-		authed: true,
-		userId,
-	}
+  return {
+    authed: true,
+    userId,
+  };
 }
