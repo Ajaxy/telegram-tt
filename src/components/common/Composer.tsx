f96798e62130b06f8af7dff1bac273ffc69aa2d7
Hao Chen <501018786@qq.com>
2024-05-10 22:36:56 +0800

    Composer: Add check for empty input to prevent errors on delete action(#363)

--- src/components/common/Composer.tsx
+++ src/components/common/Composer.tsx
@@ -1271,8 +1271,10 @@ const Composer: FC<OwnProps & StateProps> = ({
   });
 
   const removeSymbol = useLastCallback((inInputId = editableInputId) => {
-    const selection = window.getSelection()!;
+    const html = getHtml();
+    if (!html) return;
 
+    const selection = window.getSelection()!;
     if (selection.rangeCount) {
       const selectionRange = selection.getRangeAt(0);
       if (isSelectionInsideInput(selectionRange, inInputId)) {
@@ -1281,7 +1283,7 @@ const Composer: FC<OwnProps & StateProps> = ({
       }
     }
 
-    setHtml(deleteLastCharacterOutsideSelection(getHtml()));
+    setHtml(deleteLastCharacterOutsideSelection(html));
   });
 
   const removeSymbolAttachmentModal = useLastCallback(() => {
