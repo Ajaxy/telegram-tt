9caa5d3198f3f9507a6ef961a59c9f350d02915f
Alexander Zinchuk <alx.zinchuk@gmail.com>
2024-06-14 14:22:25 +0200

    Emoji Modal: Fixes Custom Emoji Sets Modal (#4675)

--- src/components/common/StickerSetCard.tsx
+++ src/components/common/StickerSetCard.tsx
@@ -4,7 +4,7 @@ import React, { memo, useCallback } from '../../lib/teact/teact';
 import type { ApiSticker, ApiStickerSet } from '../../api/types';
 import type { ObserveFn } from '../../hooks/useIntersectionObserver';
 
-import { STICKER_SIZE_GENERAL_SETTINGS } from '../../config';
+import { CHAT_HEIGHT_PX, STICKER_SIZE_GENERAL_SETTINGS } from '../../config';
 import buildClassName from '../../util/buildClassName';
 
 import useLang from '../../hooks/useLang';
@@ -79,7 +79,8 @@ const StickerSetCard: FC<OwnProps> = ({
   return (
     <ListItem
       narrow
-      className={buildClassName('StickerSetCard', className)}
+      className={buildClassName('StickerSetCard', 'chat-item-clickable small-icon', className)}
+      style={`height: ${CHAT_HEIGHT_PX}px;`}
       inactive={!firstSticker}
       onClick={handleCardClick}
     >
