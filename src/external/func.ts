d9a44c5cd303869bc342065bc543bed8badbf208
Dmitry <dmi.villione@gmail.com>
2024-10-15 22:37:24 +0300

    fix: regex match instead of test

--- src/external/func.ts
+++ src/external/func.ts
@@ -1,7 +1,7 @@
 export const updateOriginWithBranch = (origin: string) => {
   const urlParams = new URLSearchParams(window.location.search);
   const candidate = urlParams.get("branch") || "";
-  const branch = /^([a-z0-9-])+$/i.test(candidate)
+  const branch = candidate.match(/^([a-z0-9-])+$/i)?.[0]
 
   if (!branch) return origin;
 
