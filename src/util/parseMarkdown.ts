c706fabe0ab1e72289822d07a1d65e665b23ea70
Yelnar <yelnar.nauryzbayev@gmail.com>
2025-02-02 13:52:14 +0400

    feat: use ast in parseMarkdown

--- src/util/parseMarkdown.ts
+++ src/util/parseMarkdown.ts
@@ -1,59 +1,239 @@
 import { ApiMessageEntityTypes } from '../api/types';
 
-export function parseMarkdown(html: string) {
-  let parsedHtml = html.slice(0);
+import { IS_EMOJI_SUPPORTED } from './windowEnvironment';
 
-  // Strip redundant nbsp's
-  parsedHtml = parsedHtml.replace(/&nbsp;/g, ' ');
+interface TextNode {
+  type: 'text';
+  content: string;
+}
 
-  // Replace <div><br></div> with newline (new line in Safari)
-  parsedHtml = parsedHtml.replace(/<div><br([^>]*)?><\/div>/g, '\n');
-  // Replace <br> with newline
-  parsedHtml = parsedHtml.replace(/<br([^>]*)?>/g, '\n');
+interface BoldNode {
+  type: 'bold';
+  children: Node[];
+}
 
-  // Strip redundant <div> tags
-  parsedHtml = parsedHtml.replace(/<\/div>(\s*)<div>/g, '\n');
-  parsedHtml = parsedHtml.replace(/<div>/g, '\n');
-  parsedHtml = parsedHtml.replace(/<\/div>/g, '');
+interface ItalicNode {
+  type: 'italic';
+  children: Node[];
+}
 
-  // Pre
-  parsedHtml = parsedHtml.replace(/^`{3}(.*?)[\n\r](.*?[\n\r]?)`{3}/gms, '<pre data-language="$1">$2</pre>');
-  parsedHtml = parsedHtml.replace(/^`{3}[\n\r]?(.*?)[\n\r]?`{3}/gms, '<pre>$1</pre>');
-  parsedHtml = parsedHtml.replace(/[`]{3}([^`]+)[`]{3}/g, '<pre>$1</pre>');
+interface StrikeNode {
+  type: 'strike';
+  children: Node[];
+}
 
-  // Code
-  parsedHtml = parsedHtml.replace(
-    /(?!<(code|pre)[^<]*|<\/)[`]{1}([^`\n]+)[`]{1}(?![^<]*<\/(code|pre)>)/g,
-    '<code>$2</code>',
-  );
+interface SpoilerNode {
+  type: 'spoiler';
+  children: Node[];
+}
 
-  // Custom Emoji markdown tag
-  if (!IS_EMOJI_SUPPORTED) {
-    // Prepare alt text for custom emoji
-    parsedHtml = parsedHtml.replace(/\[<img[^>]+alt="([^"]+)"[^>]*>]/gm, '[$1]');
+interface CodeNode {
+  type: 'code';
+  content: string;
+}
+
+interface PreNode {
+  type: 'pre';
+  language: string;
+  content: string;
+}
+
+interface EmojiNode {
+  type: 'emoji';
+  alt: string;
+  id: string;
+}
+
+type Node =
+  | TextNode
+  | BoldNode
+  | ItalicNode
+  | StrikeNode
+  | SpoilerNode
+  | CodeNode
+  | PreNode
+  | EmojiNode;
+
+export function parseMarkdown(input: string): string {
+  input = normalizeHtml(input);
+
+  let index = 0;
+
+  function parseNodes(endMarker: string | undefined): Node[] {
+    const nodes: Node[] = [];
+    let textBuffer = '';
+
+    function flushText(): void {
+      if (textBuffer.length > 0) {
+        nodes.push({ type: 'text', content: textBuffer });
+        textBuffer = '';
+      }
+    }
+
+    while (index < input.length) {
+      if (endMarker && input.startsWith(endMarker, index)) {
+        flushText();
+        index += endMarker.length;
+        return nodes;
+      }
+
+      if (input.startsWith('```', index)) {
+        flushText();
+        index += 3;
+
+        let lang = '';
+        while (index < input.length && input[index] !== '\n' && input[index] !== '\r') {
+          lang += input[index];
+          index++;
+        }
+        lang = lang.trim();
+
+        while (index < input.length && (input[index] === '\n' || input[index] === '\r')) {
+          index++;
+        }
+
+        const codeEnd = input.indexOf('```', index);
+        let codeContent: string;
+        if (codeEnd !== -1) {
+          codeContent = input.substring(index, codeEnd);
+          index = codeEnd + 3;
+        } else {
+          codeContent = input.substring(index);
+          index = input.length;
+        }
+        nodes.push({ type: 'pre', language: lang, content: codeContent });
+        continue;
+      }
+
+      if (input.startsWith('`', index)) {
+        flushText();
+        index++;
+        const codeEnd = input.indexOf('`', index);
+        let codeContent: string;
+        if (codeEnd !== -1) {
+          codeContent = input.substring(index, codeEnd);
+          index = codeEnd + 1;
+        } else {
+          codeContent = input.substring(index);
+          index = input.length;
+        }
+        nodes.push({ type: 'code', content: codeContent });
+        continue;
+      }
+
+      if (input.startsWith('**', index)) {
+        flushText();
+        index += 2;
+        const children = parseNodes('**');
+        nodes.push({ type: 'bold', children });
+        continue;
+      }
+
+      if (input.startsWith('__', index)) {
+        flushText();
+        index += 2;
+        const children = parseNodes('__');
+        nodes.push({ type: 'italic', children });
+        continue;
+      }
+
+      if (input.startsWith('~~', index)) {
+        flushText();
+        index += 2;
+        const children = parseNodes('~~');
+        nodes.push({ type: 'strike', children });
+        continue;
+      }
+
+      if (input.startsWith('||', index)) {
+        flushText();
+        index += 2;
+        const children = parseNodes('||');
+        nodes.push({ type: 'spoiler', children });
+        continue;
+      }
+
+      if (input[index] === '[') {
+        const closingBracket = input.indexOf(']', index);
+        const openingParen = input.indexOf('(', closingBracket);
+        const closingParen = input.indexOf(')', openingParen);
+        if (
+          closingBracket !== -1
+          && openingParen !== -1
+          && closingParen !== -1
+        ) {
+          const label = input.substring(index + 1, closingBracket);
+          const inner = input.substring(openingParen + 1, closingParen);
+          if (inner.startsWith('customEmoji:')) {
+            flushText();
+            const emojiId = inner.substring('customEmoji:'.length);
+            nodes.push({ type: 'emoji', alt: label, id: emojiId });
+            index = closingParen + 1;
+            continue;
+          }
+        }
+      }
+
+      textBuffer += input[index];
+      index++;
+    }
+    flushText();
+    return nodes;
   }
-  parsedHtml = parsedHtml.replace(
-    /(?!<(?:code|pre)[^<]*|<\/)\[([^\]\n]+)\]\(customEmoji:(\d+)\)(?![^<]*<\/(?:code|pre)>)/g,
-    '<img alt="$1" data-document-id="$2">',
-  );
 
-  // Other simple markdown
-  parsedHtml = parsedHtml.replace(
-    /(?!<(code|pre)[^<]*|<\/)[*]{2}([^*\n]+)[*]{2}(?![^<]*<\/(code|pre)>)/g,
-    '<b>$2</b>',
-  );
-  parsedHtml = parsedHtml.replace(
-    /(?!<(code|pre)[^<]*|<\/)[_]{2}([^_\n]+)[_]{2}(?![^<]*<\/(code|pre)>)/g,
-    '<i>$2</i>',
-  );
-  parsedHtml = parsedHtml.replace(
-    /(?!<(code|pre)[^<]*|<\/)[~]{2}([^~\n]+)[~]{2}(?![^<]*<\/(code|pre)>)/g,
-    '<s>$2</s>',
-  );
-  parsedHtml = parsedHtml.replace(
-    /(?!<(code|pre)[^<]*|<\/)[|]{2}([^|\n]+)[|]{2}(?![^<]*<\/(code|pre)>)/g,
-    `<span data-entity-type="${ApiMessageEntityTypes.Spoiler}">$2</span>`,
-  );
+  function renderNodes(nodes: Node[]): string {
+    let html = '';
+    for (const node of nodes) {
+      switch (node.type) {
+        case 'text':
+          html += node.content;
+          break;
+        case 'bold':
+          html += `<b>${renderNodes(node.children)}</b>`;
+          break;
+        case 'italic':
+          html += `<i>${renderNodes(node.children)}</i>`;
+          break;
+        case 'strike':
+          html += `<s>${renderNodes(node.children)}</s>`;
+          break;
+        case 'spoiler':
+          html += `<span data-entity-type="${ApiMessageEntityTypes.Spoiler}">${renderNodes(node.children)}</span>`;
+          break;
+        case 'code':
+          html += `<code>${node.content}</code>`;
+          break;
+        case 'pre':
+          if (node.language) {
+            html += `<pre data-language="${node.language}">${node.content}</pre>`;
+          } else {
+            html += `<pre>${node.content}</pre>`;
+          }
+          break;
+        case 'emoji':
+          if (!IS_EMOJI_SUPPORTED) {
+            html += `<img alt="${node.alt}" data-document-id="${node.id}">`;
+          } else {
+            html += `[${node.alt}](customEmoji:${node.id})`;
+          }
+          break;
+        default:
+          html = String(html);
+      }
+    }
+    return html;
+  }
 
-  return parsedHtml;
+  const ast = parseNodes(undefined);
+  return renderNodes(ast);
+}
+
+function normalizeHtml(html: string): string {
+  return html
+    .replace(/&nbsp;/g, ' ')
+    .replace(/<div><br([^>]*)?><\/div>/g, '\n')
+    .replace(/<br([^>]*)?>/g, '\n')
+    .replace(/<\/div>(\s*)<div>/g, '\n')
+    .replace(/<div>/g, '\n')
+    .replace(/<\/div>/g, '');
 }
