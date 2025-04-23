752c6f4fab8b05490d4b22a9fda79ec8a7e35cba
zubiden <19638254+zubiden@users.noreply.github.com>
2025-04-23 18:59:39 +0200

    Emoji: Fix support check (#5868)

--- src/util/browser/windowEnvironment.ts
+++ src/util/browser/windowEnvironment.ts
@@ -135,7 +135,7 @@ function isLastEmojiVersionSupported() {
   inlineEl.classList.add('emoji-test-element');
   document.body.appendChild(inlineEl);
 
-  inlineEl.innerText = '🫆'; // Emoji from 16.0 version
+  inlineEl.innerText = '🇨🇶'; // Emoji from 16.0 version
   const newEmojiWidth = inlineEl.offsetWidth;
   inlineEl.innerText = '❤️'; // Emoji from 1.0 version
   const legacyEmojiWidth = inlineEl.offsetWidth;
