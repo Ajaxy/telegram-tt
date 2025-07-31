0d37bac02a373a1e728ccbdbceefc0126c165aef
RainbowBird <git@luoling.moe>
2025-07-31 23:43:19 +0800

    chore: remove dist

--- dist/1692.66caee8054d626d59d48.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktelegram_t=self.webpackChunktelegram_t||[]).push([[1692],{99723:e=>{e.exports=function(e){return{name:"Dust",aliases:["dst"],case_insensitive:!0,subLanguage:"xml",contains:[{className:"template-tag",begin:/\{[#\/]/,end:/\}/,illegal:/;/,contains:[{className:"name",begin:/[a-zA-Z\.-]+/,starts:{endsWithParent:!0,relevance:0,contains:[e.QUOTE_STRING_MODE]}}]},{className:"template-variable",begin:/\{/,end:/\}/,illegal:/;/,keywords:"if eq ne lt lte gt gte select default math sep"}]}}}}]);
-//# sourceMappingURL=1692.66caee8054d626d59d48.js.map
--- dist/9295.a6b850b7b7099199fa9c.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunktelegram_t=self.webpackChunktelegram_t||[]).push([[9295],{58942:e=>{e.exports=function(e){return{name:"Test Anything Protocol",case_insensitive:!0,contains:[e.HASH_COMMENT_MODE,{className:"meta",variants:[{begin:"^TAP version (\\d+)$"},{begin:"^1\\.\\.(\\d+)$"}]},{begin:/---$/,end:"\\.\\.\\.$",subLanguage:"yaml",relevance:0},{className:"number",begin:" (\\d+) "},{className:"symbol",variants:[{begin:"^ok"},{begin:"^not ok"}]}]}}}}]);
-//# sourceMappingURL=9295.a6b850b7b7099199fa9c.js.map
