2781b6032acafd563745876ce6e9ae7fbd3f2539
zubiden <19638254+zubiden@users.noreply.github.com>
2025-12-08 17:39:23 +0100

    Message List: Fix bottom snap not applying before scroll (#6508)

--- src/components/middle/MessageList.tsx
+++ src/components/middle/MessageList.tsx
@@ -742,6 +742,8 @@ const MessageList = ({
 
           requestMeasure(() => {
             isScrollTopJustUpdatedRef.current = false;
+
+            updateBottomSnapClass();
           });
         }
 
