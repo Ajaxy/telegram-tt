eb9f74669d8f855c5e8c6cd4879a0a629a745811
Alexander Zinchuk <alx.zinchuk@gmail.com>
2026-07-01 14:12:40 +0200

    [Build]

---
+++ dist/assets/dist-js-BXpYhKlV.js
@@ -0,0 +1,2 @@
+import{yv as e}from"./shared-components-C_ybLBTv.js";async function t(){await e(`plugin:process|restart`)}export{t as relaunch};
+//# sourceMappingURL=dist-js-BXpYhKlV.js.map
--- dist/assets/dist-js-CggZTaBU.js
+++
@@ -1,2 +0,0 @@
-import{yv as e}from"./shared-components-CITs13Hz.js";async function t(){await e(`plugin:process|restart`)}export{t as relaunch};
-//# sourceMappingURL=dist-js-CggZTaBU.js.map
