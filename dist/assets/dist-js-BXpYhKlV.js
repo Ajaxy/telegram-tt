171fb56a2d45cee6f50d89975e5bf26cc12e5de0
Alexander Zinchuk <alx.zinchuk@gmail.com>
2026-07-15 01:46:53 +0200

    [Build]

--- dist/assets/dist-js-BXpYhKlV.js
+++
@@ -1,2 +0,0 @@
-import{yv as e}from"./shared-components-C_ybLBTv.js";async function t(){await e(`plugin:process|restart`)}export{t as relaunch};
-//# sourceMappingURL=dist-js-BXpYhKlV.js.map
---
+++ dist/assets/encoderWorker.min-C37CwOLG.js
@@ -0,0 +1,2 @@
+var e=``+new URL(`encoderWorker.min-BL5medTV.js`,import.meta.url).href;export{e as default};
+//# sourceMappingURL=encoderWorker.min-C37CwOLG.js.map
