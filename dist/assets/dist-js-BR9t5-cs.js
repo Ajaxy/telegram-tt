a323b1f29f5b91e9a32b8c14866234e925fb0bfa
Alexander Zinchuk <alx.zinchuk@gmail.com>
2026-07-30 15:40:15 +0200

    [Build]

--- dist/assets/dist-js-BR9t5-cs.js
+++
@@ -1,2 +0,0 @@
-import{Mv as e}from"./shared-components-BsvtxSL0.js";async function t(){await e(`plugin:process|restart`)}export{t as relaunch};
-//# sourceMappingURL=dist-js-BR9t5-cs.js.map
---
+++ dist/assets/dist-js-DHMu1xdK.js
@@ -0,0 +1,2 @@
+import{Mv as e}from"./shared-components-C06lLLsV.js";async function t(){await e(`plugin:process|restart`)}export{t as relaunch};
+//# sourceMappingURL=dist-js-DHMu1xdK.js.map
