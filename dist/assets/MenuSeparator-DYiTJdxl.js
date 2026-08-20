8b63941b230b3870accc442b5ef5ac95fc53c719
Alexander Zinchuk <alx.zinchuk@gmail.com>
2026-08-20 11:25:23 +0200

    [Build]

---
+++ dist/assets/MenuSeparator-DYiTJdxl.js
@@ -0,0 +1,2 @@
+import{lp as e,ou as t}from"./InputText-CjcM7Vjc.js";var n={root:`_40aWbFdG`,thin:`_1yj96yuC`,thick:`_5EdrUuc8`},r=({className:r,size:i=`thin`})=>e(`div`,{className:t(n.root,n[i],r)});export{r as t};
+//# sourceMappingURL=MenuSeparator-DYiTJdxl.js.map
